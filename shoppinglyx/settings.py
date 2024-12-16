95ba1bc9516e7c76123585abdd10cc7dc304a1fc
jamal20 <mdjamaluddinh15@gmail.com>
2024-12-16 17:10:40 +0600

    add settings

--- shoppinglyx/settings.py
+++ shoppinglyx/settings.py
@@ -132,8 +132,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Media files (Uploaded content)
 MEDIA_URL = '/media/'
