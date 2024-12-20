01cf7eeee09a4e5afd47fe5c0575f059b11391c7
jamal20 <mdjamaluddinh15@gmail.com>
2024-12-20 08:49:23 +0600

    settings add

--- shoppinglyx/urls.py
+++ shoppinglyx/urls.py
@@ -1,8 +1,14 @@
-
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('app.urls'))
+    path('', include('app.urls')),  # মূল অ্যাপের URLs
 ]
+
+# Static এবং Media ফাইলগুলি শুধুমাত্র DEBUG মোডে সার্ভ করার জন্য
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
